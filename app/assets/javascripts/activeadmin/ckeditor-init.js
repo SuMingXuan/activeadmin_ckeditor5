e7fb9e35adfa0b2c3656a41a7231f09b6613d13c
MingXuanSu <mxsu@jihulab.com>
2023-01-12 14:34:23 +0800

    Support for multiple editors

--- app/assets/javascripts/activeadmin/ckeditor-init.js
+++ app/assets/javascripts/activeadmin/ckeditor-init.js
@@ -2,9 +2,27 @@
 
 (function () {
   function initCkeditors() {
-    ClassicEditor.create(
-      document.querySelector("[data-activeadmin-ckcontent]")
-    );
+    const activeAdminCKEditor = document.querySelectorAll("[data-activeadmin-ckeditor]")
+    for (let i = 0; i < activeAdminCKEditor.length; i++) {
+      ClassicEditor.create(activeAdminCKEditor[i]);
+    }
+    const formtastic = document.querySelector("form.formtastic");
+    if (formtastic) {
+      formtastic.onsubmit = () => {
+        const editors = document.querySelectorAll(".ck-editor");
+        for (let i = 0; i < editors.length; i++) {
+          const input = activeAdminCKEditor[i].querySelector('input[type="hidden"]');
+          const editorContent = editors[i].querySelector(
+            ".ck-editor__main .ck-content"
+          );
+          if (editorContent) {
+            input.value = editorContent.innerHTML;
+          } else {
+            input.value = "";
+          }
+        }
+      }
+    }
   }
   $(document).ready(initCkeditors);
   $(document).on("has_many_add:after", ".has_many_container", initCkeditors);
--- lib/activeadmin/ckeditor/version.rb
+++ lib/activeadmin/ckeditor/version.rb
@@ -2,7 +2,6 @@
 
 module Activeadmin
   module Ckeditor
-    VERSION = "0.1.0"
-    CKEDITOR = '0.1.1'
+    VERSION = "0.1.1"
   end
 end
