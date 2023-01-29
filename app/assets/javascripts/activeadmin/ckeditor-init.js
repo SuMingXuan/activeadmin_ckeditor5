b7e6b7dc7b1b1896dc1213fadc96a879468882fc
MingXuanSu <mxsu@jihulab.com>
2023-01-29 22:38:26 +0800

    Fix display content and css

--- app/assets/javascripts/activeadmin/ckeditor-init.js
+++ app/assets/javascripts/activeadmin/ckeditor-init.js
@@ -2,39 +2,38 @@
 
 (function () {
   function initCkeditors() {
-    const activeAdminCKEditor = document.querySelectorAll("[data-activeadmin-ckeditor]")
+    const activeAdminCKEditor = document.querySelectorAll(
+      "[data-activeadmin-ckeditor]"
+    );
     for (let i = 0; i < activeAdminCKEditor.length; i++) {
-      ClassicEditor.create(activeAdminCKEditor[i]);
-    }
-    const formtastic = document.querySelector("form.formtastic");
-    if (formtastic) {
-      formtastic.onsubmit = () => {
-        const editors = document.querySelectorAll(".ck-editor");
-        for (let i = 0; i < editors.length; i++) {
-          const input = activeAdminCKEditor[i].querySelector('input[type="hidden"]');
-          let editor = editors[i]
-          removeFakeContainer(editor)
-          const editorContent = editor.querySelector(
-            ".ck-editor__main .ck-content"
-          );
-          if (editorContent) {
-            input.value = editorContent.innerHTML;
-          } else {
-            input.value = "";
-          }
-        }
-      }
+      let editor;
+      const input = activeAdminCKEditor[i].querySelector(
+        'input[type="hidden"]'
+      );
+
+      ClassicEditor.create(activeAdminCKEditor[i]).then((newEditor) => {
+        editor = newEditor;
+        editor.setData(input.value);
+      });
+      document.querySelector("form.formtastic").onsubmit = () => {
+        let editorElement = document.querySelectorAll(".ck-editor")[i];
+        removeFakeContainer(editorElement);
+        console.log("editor.getData()", editor.getData());
+        input.value = editor.getData();
+      };
     }
   }
 
   function removeFakeContainer(editor) {
-    const fakeContainers = editor.querySelectorAll('.ck-fake-selection-container')
-    
+    const fakeContainers = editor.querySelectorAll(
+      ".ck-fake-selection-container"
+    );
+
     for (let i = 0; i < fakeContainers.length; i++) {
-      fakeContainers[i].remove()
+      fakeContainers[i].remove();
     }
   }
-  
+
   $(document).ready(initCkeditors);
   $(document).on("has_many_add:after", ".has_many_container", initCkeditors);
   $(document).on("turbolinks:load", initCkeditors);
